66743636899e3427cbaa29084e3d9588cce914a4
Klaudia Prymula <klaudia.prymula@microsoft.wsei.edu.pl>
2026-02-11 22:44:58 +0100

    Initialize Spring Boot Gradle project (1)

---
+++ horse-breeds/src/main/java/com/example/horsebreeds/HorseBreedsApplication.java
@@ -0,0 +1,13 @@
+package com.example.horsebreeds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorseBreedsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HorseBreedsApplication.class, args);
+    }
+
+}
